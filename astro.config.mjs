e6c361491adebf6fe63750cfed0887b05940b8ab
melissa <melissa.iman.n@uni.pe>
2025-05-07 18:21:06 -0500

    fix: image issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,13 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind()],
+    // Añadir base para GitHub Pages - se asume que el repositorio es "en-mancha-landing"
+    base: '/en-mancha-landing',
+    // Configuración para optimización de imágenes
+    image: {
+        // Habilitar el servicio de optimización de imágenes
+        service: { 
+            entrypoint: 'astro/assets/services/sharp' 
+        },
+    },
 });
